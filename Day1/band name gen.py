556df7d2164153eae075ef0443cf99a2d8ddf2c4
akashyap25 <anuragkashyap026@gmail.com>
2022-12-28 11:37:55 +0530

    till day 3

---
+++ Day1/band name gen.py
@@ -0,0 +1,16 @@
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
+print("Welcome to band name generator")
+city= input("What is your city?\n")
+pet_name=input("What is your pet name?\n")
+
+print(city+" "+pet_name)
---
+++ day3/treasure-island.py
@@ -0,0 +1,52 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.\n There are 3 levels.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+count=0;
+
+temp= input("Choose Left or Right, type L or R: \n")
+if(temp=='L'):
+  temp=input("Choose Swim or wait, type S or W:\n")
+  count+=1;
+  if(temp=='W'):
+    temp=input("You choose which door Red, Blue or Yellow, type R, B or Y\n")
+    count+=1
+    if(temp=='B'):
+      print(f"Eaten by beasts.\n Game over. Score {count}")
+    elif temp=='R':
+      print(f"Burnt by fire.\n Game over. Score {count}")
+    elif temp=='Y':
+      count+=1
+      print("You win!, All 3 levels cleared.")
+    else:
+      print(f"Game over. Score: {count}")
+      
+      
+  else:
+    print(f"Attacked by trout.\n Game over. Score: {count}")
+else:
+  print(f"Fall into a hole.\n Game over. Score: {count}")
